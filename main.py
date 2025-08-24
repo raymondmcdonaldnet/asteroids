a7544a72dba9c5a2fe5f199d3190a160f362e86c
R.L. McDonald <me@rlmcdonald.com>
2025-08-24 03:27:15 -0500

    Implement a fixed timestep and save delta time (`dt`)

--- main.py
+++ main.py
@@ -5,15 +5,22 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     # Main game loop
     while True:
+        # Handle events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # Draw the game.
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        # Convert delta time to seconds and store it.
+        dt = clock.tick(TARGET_FPS) / 1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
