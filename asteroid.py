89b93213787f683af1f1760a7a1889c933a0b065
R.L. McDonald <me@rlmcdonald.com>
2025-08-24 04:21:13 -0500

    Implement asteroid splitting on death

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 from circleshape import CircleShape
 import pygame
+from constants import ASTEROID_MIN_RADIUS
+import random
 
 class Asteroid(CircleShape):
 	def __init__(self, x, y, radius):
@@ -10,3 +12,21 @@ class Asteroid(CircleShape):
 
 	def update(self, dt):
 		self.position += (self.velocity * dt)
+
+	def split(self):
+		self.kill()
+		# Already done with small asteroids.
+		if self.radius <= ASTEROID_MIN_RADIUS:
+			return
+		# Larger asteroids need to spawn new asteroids on death.
+		else:
+			spawn_angle = random.uniform(20, 50)
+			new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+			left_hand_asteroid_velocity = self.velocity.rotate(-spawn_angle) * 1.2
+			left_hand_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+			left_hand_asteroid.velocity = left_hand_asteroid_velocity
+
+			right_hand_asteroid_velocity = self.velocity.rotate(spawn_angle) * 1.2
+			right_hand_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+			right_hand_asteroid.velocity = right_hand_asteroid_velocity
--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def main():
             for shot in shots:
                 if asteroid.check_collision(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         # Convert delta time to seconds and store it.
         dt = clock.tick(TARGET_FPS) / 1000
